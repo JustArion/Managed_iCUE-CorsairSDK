68f83c818b3da01fccd1db5ebeaded20d7a1dede
arion <53127594+JustArion@users.noreply.github.com>
2024-10-26 14:25:55 +0200

    Include other access levels

--- src/CorsairSDK/Lighting/AccessLevel.cs
+++ src/CorsairSDK/Lighting/AccessLevel.cs
@@ -2,8 +2,20 @@
 
 public enum AccessLevel
 {
+    /// <summary>
+    /// Shared mode. Other applications can set Leds
+    /// </summary>
     Shared = 0,
+    /// <summary>
+    /// Exclusive mode. Only this specific application can set Leds (Disconnects other applications from iCUE)
+    /// </summary>
     Exclusive = 1,
-    // ExclusiveKeyEventsListening = 2,
-    // ExclusiveLightingControlAndKeyEventsListening = 3,
+    /// <summary>
+    /// Exclusive key events, but shared lightings
+    /// </summary>
+    ExclusiveKeyEventsListening = 2,
+    /// <summary>
+    /// Fully exclusive mode
+    /// </summary>
+    ExclusiveLightingControlAndKeyEventsListening = 3,
 }
